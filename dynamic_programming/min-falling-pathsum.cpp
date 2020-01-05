f68b73df9073e6658719a8eca9988bd3f061e1b8
Suraj Patni <patnisrj22@gmail.com>
2020-01-05 16:11:39 +0530

    by recursion

---
+++ dynamic_programming/min-falling-pathsum.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    const int inf=1e8;
+    int start(int x,int y,vector<vector<int>> &a)
+    {
+        if(x==a.size()) return 0;
+        if(y==-1 || y==a[0].size()) return inf;
+
+
+        int ans=INT_MAX;
+        ans = min(ans,a[x][y] + start(x+1,y-1,a));
+        ans = min(ans,a[x][y] + start(x+1,y,a));
+        ans = min(ans,a[x][y] + start(x+1,y+1,a));
+
+        return ans;
+    }
+
+    int minFallingPathSum(vector<vector<int>>& a) {
+        int row = a.size();
+        int col = a[0].size();
+
+        int ans = INT_MAX;
+        for(int i=0;i<col;i++)
+        {
+            ans  = min(ans,start(0,i,a));
+        }
+        return ans;
+    }
+}; 
