d9fdff65ff7fd7203b00ae33a8597762748fe456
suraj patni <patnisrj22@gmail.com>
2020-01-17 11:40:46 +0530

    doing by bottom-up instead

--- dynamic_programming/climbing-stairs.cpp
+++ dynamic_programming/climbing-stairs.cpp
@@ -2,17 +2,13 @@
 
 class Solution {
 public:
-    vector<int> dp;
-    int go(int n)
-    {
-        if(n<0)     return 0;
-        if(n==0)    return 1;
-        if(dp[n]!=-1)   return dp[n];
-        return dp[n] = go(n-1) + go(n-2);
-        
-    }
+    
     int climbStairs(int n) {
-        dp.resize(n+1,-1);
-        return go(n);
+        vector<int> dp(n+1);
+        dp[0] = dp[1] = 1;
+        
+        for(int i=2;i<=n;i++)
+            dp[i] = dp[i-1] + dp[i-2];
+        return dp[n];
     }
 };
