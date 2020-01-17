9fe9ea6db4fd45865b7392914c7f00c60d42b103
suraj patni <patnisrj22@gmail.com>
2020-01-17 13:31:19 +0530

    is subsequence problem applied memoization

--- dynamic_programming/is-subsequence.cpp
+++ dynamic_programming/is-subsequence.cpp
@@ -2,19 +2,22 @@
 
 class Solution {
 public:
+    vector<vector<int>> dp;
     bool go(int n,int m,string &s, string &t)
     {
-        if(m==-1)   return true;   // child string become empty.
-        if(n==-1)   return false;  // parent string become empty
+        if(m==-1)   return 1;   // child string become empty.
+        if(n==-1)   return 0;  // parent string become empty
         
-        if(t[n] != s[m])    return go(n-1,m,s,t);
+        if(dp[n][m] != -1)  return dp[n][m];
         
-        return go(n-1,m-1,s,t) || go(n-1,m,s,t);
+        if(t[n] != s[m])    return dp[n][m] = go(n-1,m,s,t);
+        
+        return dp[n][m] = go(n-1,m-1,s,t) || go(n-1,m,s,t);
     }
     bool isSubsequence(string s, string t) {
         int n = t.size();
         int m = s.size();
-        
-       return go(n,m,s,t); 
+        dp.resize(n+1,vector<int>(m+1,-1));
+        return go(n,m,s,t); 
     }
 };
