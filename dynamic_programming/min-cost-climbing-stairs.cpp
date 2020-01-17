07c80f52d2fe973b9cd6a0e6a2c72031a7582f8c
suraj patni <patnisrj22@gmail.com>
2020-01-17 12:40:21 +0530

    min cost climbing staircase applied memoization

--- dynamic_programming/min-cost-climbing-stairs.cpp
+++ dynamic_programming/min-cost-climbing-stairs.cpp
@@ -1,13 +1,17 @@
 // https://leetcode.com/problems/min-cost-climbing-stairs/
 class Solution {
 public:
+    vector<int> dp;
     int go(int index,vector<int> &cost)
     {
         if(index==0 || index==1)    return cost[index];
-        return cost[index] + min(go(index-1,cost),go(index-2,cost));
+        if(dp[index] != -1) return dp[index];
+        return dp[index] = cost[index] + min( go(index-1,cost),go(index-2,cost) );
     }
     int minCostClimbingStairs(vector<int>& cost) {
         int n = cost.size();
+        dp.resize(n+1,-1);
+        
         cost.push_back(0);
         return go(n,cost);
     }
