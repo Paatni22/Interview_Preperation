5c1a55942e36622a69c37a7e6e036c45cc67b795
suraj patni <patnisrj22@gmail.com>
2020-01-17 13:49:07 +0530

    is subsequence problem by bottom-up

--- dynamic_programming/is-subsequence.cpp
+++ dynamic_programming/is-subsequence.cpp
@@ -2,22 +2,22 @@
 
 class Solution {
 public:
-    vector<vector<int>> dp;
-    bool go(int n,int m,string &s, string &t)
-    {
-        if(m==-1)   return 1;   // child string become empty.
-        if(n==-1)   return 0;  // parent string become empty
-        
-        if(dp[n][m] != -1)  return dp[n][m];
-        
-        if(t[n] != s[m])    return dp[n][m] = go(n-1,m,s,t);
-        
-        return dp[n][m] = go(n-1,m-1,s,t) || go(n-1,m,s,t);
-    }
     bool isSubsequence(string s, string t) {
         int n = t.size();
         int m = s.size();
-        dp.resize(n+1,vector<int>(m+1,-1));
-        return go(n,m,s,t); 
+        vector<vector<bool>> dp(n+1,vector<bool>(m+1,false));
+        for(int i=0;i<=m+1;i++)
+            dp[0][i] = true;
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=m;j++)
+            {
+                if(t[i-1] != s[i-1])
+                    dp[i][j] = dp[i-1][j];
+                else
+                dp[i][j] = dp[i-1][j-1] || dp[i-1][j];
+            }
+        }
+        return dp[n][m];
     }
 };
