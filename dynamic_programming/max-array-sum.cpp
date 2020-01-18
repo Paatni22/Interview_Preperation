f97b5a85884b65eef9ccdd51c7b584804c026f11
Suraj Patni <patnisrj22@gmail.com>
2020-01-18 11:41:22 +0530

    max-array-subset by bottom-up

--- dynamic_programming/max-array-sum.cpp
+++ dynamic_programming/max-array-sum.cpp
@@ -1,16 +1,13 @@
 //https://www.hackerrank.com/challenges/max-array-sum/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=dynamic-programming
-vector<int> dp;
-int maxSum(int i,vector<int> &arr)
-{
-  if(i<0) return 0;
-  if(dp[i] != -1)	return dp[i];
-  if(arr[i]<0)  return dp[i] = maxSum(i-1,arr);
 
-  return dp[i] = max( arr[i]+maxSum(i-2,arr), maxSum(i-1,arr) );
-}
 // Complete the maxSubsetSum function below.
 int maxSubsetSum(vector<int> arr) {
   int n = arr.size();
-  dp.resize(n,-1);
-  return maxSum(n-1,arr);
+  vector<int> dp(n+1);
+  dp[0] = 0;
+  dp[1] = (arr[0]<0 ? 0 : arr[0]);
+  for(int i=2;i<=n;i++)
+  	dp[i] = max(arr[i-1]+dp[i-2],dp[i-1]);
+
+  return dp[n];
 }
