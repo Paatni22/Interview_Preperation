150feaf563f1560b139f47f47707f15f963797d5
Suraj Patni <patnisrj22@gmail.com>
2020-01-18 11:07:48 +0530

    max-array-subset applied memoization

--- dynamic_programming/max-array-sum.cpp
+++ dynamic_programming/max-array-sum.cpp
@@ -1,14 +1,16 @@
 //https://www.hackerrank.com/challenges/max-array-sum/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=dynamic-programming
-
+vector<int> dp;
 int maxSum(int i,vector<int> &arr)
 {
   if(i<0) return 0;
-  if(arr[i]<0)  return maxSum(i-1,arr);
+  if(dp[i] != -1)	return dp[i];
+  if(arr[i]<0)  return dp[i] = maxSum(i-1,arr);
 
-  return max( arr[i]+maxSum(i-2,arr), maxSum(i-1,arr) );
+  return dp[i] = max( arr[i]+maxSum(i-2,arr), maxSum(i-1,arr) );
 }
 // Complete the maxSubsetSum function below.
 int maxSubsetSum(vector<int> arr) {
   int n = arr.size();
+  dp.resize(n,-1);
   return maxSum(n-1,arr);
 }
