a5d7dd3eb8d90b29709a5c929188cb3327db972a
Suraj Patni <patnisrj22@gmail.com>
2020-01-19 19:16:39 +0530

    counting-bits by dp bottom-up

---
+++ dynamic_programming/counting-bits.cpp
@@ -0,0 +1,15 @@
+// https://leetcode.com/problems/counting-bits/submissions/
+
+class Solution {
+public:
+    vector<int> countBits(int num) {
+        vector<int> dp(num+1,0);  
+        
+        dp[0] = 0;
+        
+        for(int i=1;i<=num;i++)  
+            dp[i] = dp[i>>1] + (i&1);
+       
+        return dp;
+    }
+};
