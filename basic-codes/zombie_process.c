e450171ef121d54095ea6b3092e701c7136945bf
Suraj Patni <patnisrj22@gmail.com>
2020-09-12 14:51:55 +0530

    os fork,zombie program

--- basic-codes/zombie_process.c
+++ basic-codes/zombie_process.c
@@ -1,3 +1,6 @@
+// checking working of zombie process. 
+// Run "top" command in parallel terminal to see 
+// no of zombie proceses at realtime.
 #include<stdio.h>
 #include<unistd.h>  // for fork() and sleep().
 #include<stdlib.h> // for exit().
@@ -11,7 +14,12 @@ int main()
 	}
 	else
 	{
+		//parent sleeps for 5 sec. but child immediately 
+		// exits, so child entry remains in process table for 
+		// 5 secs, so a zombie process created for 5 secs.
 		sleep(5);  // in seconds.
+		// here parent resumes and reaps off(remove) child entry
+		// from process table.
 	}
 }
 
