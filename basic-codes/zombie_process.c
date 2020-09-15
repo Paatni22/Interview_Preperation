59012d5dd3c04fad1208728b0873c9493ce8d828
Suraj Patni <patnisrj22@gmail.com>
2020-09-15 12:12:21 +0530

    added wait() function

--- basic-codes/zombie_process.c
+++ basic-codes/zombie_process.c
@@ -5,19 +5,21 @@
 #include<unistd.h>  // for fork() and sleep().
 #include<stdlib.h> // for exit().
 #include<sys/wait.h> // for wait(). wait() not used here.
+//    wait(NULL) -> calling this in parent, will make parent to 
+//                   wait for next 1 child. then resume.
 int main()
 {
 	int id = fork();
-	if(id==0)
+	if(id==0)  // child
 	{
 		exit(0);
 	}
-	else
+	else     // parent
 	{
 		//parent sleeps for 5 sec. but child immediately 
 		// exits, so child entry remains in process table for 
 		// 5 secs, so a zombie process created for 5 secs.
-		sleep(5);  // in seconds.
+		sleep(5);  // 5 in seconds.
 		// here parent resumes and reaps off(remove) child entry
 		// from process table.
 	}
