554f69f52ec550c9e3f37d9680332da509129180
Suraj Patni <patnisrj22@gmail.com>
2021-06-05 22:17:02 +0530

    changes in schedule

---
+++ 450_DSA_questions/1.cpp
@@ -0,0 +1,20 @@
+//User function Template for C++
+
+void swap(char &a,char &b)
+{
+  char temp = a;
+  a = b;
+  b = temp;
+}
+string reverseWord(string str){
+    
+  //Your code here
+  int n = str.size();
+  for(int i=0,j=n-1; i<j; i++,j--)
+  {
+      swap(str[i],str[j]);
+  }
+  return str;
+    
+}
+
